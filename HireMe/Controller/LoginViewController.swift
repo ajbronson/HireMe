04def23b8709a629ca620294ccf3c6c78980d4cd
Nathan M Johnson <nate8etan@gmail.com>
2017-01-17 20:54:54 -0700

    LoginViewController: added touchesBegan(_:with:) to hide keyboard when user taps outside of text fields; storyboard-LoginViewController: changed 'Login' to 'Sign in'

--- HireMe/Controller/LoginViewController.swift
+++ HireMe/Controller/LoginViewController.swift
@@ -9,5 +9,39 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     
+    // MARK: - View controller life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    //This will dismiss the keyboard and resign any UITextField as first responder when the user taps outside of the text fields
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+        super.touchesBegan(touches, with: event)
+    }
+    
+    
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "showTabs") {
+            print("preparing...")
+        }
+    }
+    
+    
+    // MARK: - IBActions
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+        print("login")
+    }
+    
+    @IBAction func signUpTapped(_ sender: UIButton) {
+        print("sign up")
+    }
 }
