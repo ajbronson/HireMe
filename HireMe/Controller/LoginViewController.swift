ebc9e0cceeef443bb46c3f951166ab6733463908
Nathan M Johnson <nate8etan@gmail.com>
2017-01-17 20:02:34 -0700

    Created LoginViewController; storyboard-LoginViewController: added and adjusted constraints to show on smaller screen sizes

---
+++ HireMe/Controller/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  HireMe
+//
+//  Created by Nathan Johnson on 1/17/17.
+//  Copyright © 2017 AJ Bronson. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+}
