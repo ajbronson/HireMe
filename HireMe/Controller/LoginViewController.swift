2825f4f8cccbb8372c6be180055d5f2843c642a9
Nathan M Johnson <nate8etan@gmail.com>
2017-01-17 21:24:51 -0700

    LoginViewController: added outlets for two constraints to help with layout when orientation turns landscape; storyboard: changed 'Sign In' back to 'Log In' and 'Sign Up' to 'Create an account'

--- HireMe/Controller/LoginViewController.swift
+++ HireMe/Controller/LoginViewController.swift
@@ -9,8 +9,19 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    // MARK: - Cosntants
+    let BOTTOM_SPACING_PORTRAIT: CGFloat = 200
+    let BOTTOM_SPACING_LANDSCAPE: CGFloat = 15
+    let VERTICAL_SPACING_PORTRAIT: CGFloat = 30
+    let VERTICAL_SPACING_LANDSCAPE: CGFloat = 15
+    
+    // MARK: - Outlets
+    
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signUpBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var verticalSpacingBetweenButtons: NSLayoutConstraint!
     
     // MARK: - View controller life cycle
     
@@ -25,6 +36,15 @@ class LoginViewController: UIViewController {
         super.touchesBegan(touches, with: event)
     }
     
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        if UIDevice.current.orientation.isLandscape {
+            self.signUpBottomConstraint.constant = BOTTOM_SPACING_LANDSCAPE
+            self.verticalSpacingBetweenButtons.constant = VERTICAL_SPACING_LANDSCAPE
+        } else {
+            self.signUpBottomConstraint.constant = BOTTOM_SPACING_PORTRAIT
+            self.verticalSpacingBetweenButtons.constant = VERTICAL_SPACING_PORTRAIT
+        }
+    }
     
     // MARK: - Navigation
     
