91671934311803ae006d3a7e33299d980d35b1f0
Nathan M Johnson <nate8etan@gmail.com>
2017-01-20 18:11:50 -0700

    Created ContainerViewController, MenuViewController, and ClientTabBarController; ContainerViewController: has basic logic to create a slide-out menu from the left side; storyboard: ContainerViewController is initial view controller, copied BidsViewController and embedded it in ClientTabBarController which is embedded in a nav controller

---
+++ HireMe/Controller/ClientTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  ClientTabBarController.swift
+//  HireMe
+//
+//  Created by Nathan Johnson on 1/20/17.
+//  Copyright © 2017 AJ Bronson. All rights reserved.
+//
+
+import UIKit
+
+class ClientTabBarController: UITabBarController {
+
+    @IBAction func menuTapped(_ sender: UIBarButtonItem) {
+        print("Menu please!")
+    }
+    
+}
---
+++ HireMe/Controller/MenuViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.swift
+//  HireMe
+//
+//  Created by Nathan Johnson on 1/20/17.
+//  Copyright © 2017 AJ Bronson. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UITableViewController {
+    
+}
