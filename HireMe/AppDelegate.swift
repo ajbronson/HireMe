9936af042b02cff0612a2b56fef0dc7011615494
Nathan M Johnson <nate8etan@gmail.com>
2017-03-03 00:27:28 -0700

    AppDelegate: removed property storyboard

--- HireMe/AppDelegate.swift
+++ HireMe/AppDelegate.swift
@@ -15,7 +15,6 @@ import GoogleSignIn
 class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
 
 	var window: UIWindow?
-    let storyboard = UIStoryboard(name: "ProviderStoryboard", bundle: Bundle.main)
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		
