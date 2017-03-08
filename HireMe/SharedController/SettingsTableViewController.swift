f847c107affe7b7710f4e95faed9a911c26a3280
Nathan M Johnson <nate8etan@gmail.com>
2017-03-07 21:23:07 -0700

    MenuStoryboard: changed table view to static cells for SettingsTableViewController and changed reuse IDs, removed all segues between menu and menu options; SettingsTableViewController: removed all UITableViewDataSource functions

--- HireMe/SharedController/SettingsTableViewController.swift
+++ HireMe/SharedController/SettingsTableViewController.swift
@@ -10,38 +10,6 @@ import FBSDKLoginKit
 import GoogleSignIn
 
 class SettingsTableViewController: UITableViewController {
-
-    // MARK: - UITableViewDataSource
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        switch indexPath.section {
-            case 1:
-                let cell = tableView.dequeueReusableCell(withIdentifier: "SignInOutCell", for: indexPath)
-                
-                if SignInHelper.isSignedIn() {
-                    cell.textLabel?.text = "Sign Out"
-                    cell.textLabel?.textColor = .red
-                } else {
-                    cell.textLabel?.text = "Sign In"
-                    cell.textLabel?.textColor = .black
-                }
-                
-                return cell
-            default:
-                let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsCell", for: indexPath)
-                
-                return cell
-        }
-    }
-    
     
     // MARK: - UITableViewDelegate
     
@@ -50,39 +18,34 @@ class SettingsTableViewController: UITableViewController {
         
         let reuseId = tableView.cellForRow(at: indexPath)?.reuseIdentifier
         
-        if reuseId == "SignInOutCell" {
-            if SignInHelper.isSignedIn() {
-                let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-                alertController.popoverPresentationController?.sourceView = self.view
-                alertController.popoverPresentationController?.sourceRect = self.view.bounds;
-                alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-                alertController.addAction(UIAlertAction(title: "Sign Out", style: .destructive, handler: { (action) in
-                    self.dismiss(animated: true, completion: {
-                        print("Signing out...") // DEBUG
-                        switch SignInHelper.getSignInMethod() {
-                            case .Facebook:
-                                FBSDKLoginManager().logOut()
-                                print("Signed out from Facebook") // DEBUG
-                            case .Google:
-                                GIDSignIn.sharedInstance().signOut()
-                                print("Signed out from Google") // DEBUG
-                            case .ThisApp:
-                                print("Signed out from LimitedHire") // DEBUG
-                            default:
-                                print("Not signed in") // DEBUG
-                                break
-                        }
-                        
-                        NotificationCenter.default.post(name: signOutNotificationName, object: nil)
-                    })
-                }))
-                
-                self.present(alertController, animated: true, completion: nil)
-            } else {
-                self.performSegue(withIdentifier: "showSignIn", sender: nil)
-            }
+        if reuseId == "signOutCell" {
+            let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            alertController.popoverPresentationController?.sourceView = self.view
+            alertController.popoverPresentationController?.sourceRect = self.view.bounds;
+            alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            alertController.addAction(UIAlertAction(title: "Sign Out", style: .destructive, handler: { (action) in
+                self.dismiss(animated: true, completion: {
+                    print("Signing out...") // DEBUG
+                    switch SignInHelper.getSignInMethod() {
+                    case .Facebook:
+                        FBSDKLoginManager().logOut()
+                        print("Signed out from Facebook") // DEBUG
+                    case .Google:
+                        GIDSignIn.sharedInstance().signOut()
+                        print("Signed out from Google") // DEBUG
+                    case .ThisApp:
+                        print("Signed out from LimitedHire") // DEBUG
+                    default:
+                        print("Not signed in") // DEBUG
+                        break
+                    }
+                    
+                    NotificationCenter.default.post(name: signOutNotificationName, object: nil)
+                })
+            }))
+            
+            self.present(alertController, animated: true, completion: nil)
         }
-
     }
     
     
