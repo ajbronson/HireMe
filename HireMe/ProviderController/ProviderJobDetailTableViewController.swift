94da25021bf274a58eed2de24d7f3232c04a15a3
Nathan M Johnson <nate8etan@gmail.com>
2017-03-19 00:00:09 -0600

    ProviderJobDetailTableViewController: now shows its toolbar

--- HireMe/ProviderController/ProviderJobDetailTableViewController.swift
+++ HireMe/ProviderController/ProviderJobDetailTableViewController.swift
@@ -28,6 +28,16 @@ class ProviderJobDetailTableViewController: UITableViewController {
         tableView.rowHeight = UITableViewAutomaticDimension
         initializeTableViewData()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setToolbarHidden(false, animated: false)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.setToolbarHidden(true, animated: false)
+    }
 
     // MARK: - UITableViewDataSource
 
