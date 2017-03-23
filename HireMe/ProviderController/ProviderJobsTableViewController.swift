44961fceda816bf2bf8fce35a35e21ffd7635104
Nathan M Johnson <nate8etan@gmail.com>
2017-03-22 22:27:18 -0600

    ProviderStoryboard-ProviderJobsTableViewController: each row now segues to ProviderJobDetailTableViewController; ProviderJobsTableViewController: created a single data structure tableViewData to hold all information about what the table will need; created initializeTableViewData(), addTableViewDataSection(title:with:), tableViewDataSection(title:), tableViewDataRow(for:), job(atIndexPath:); deleted obsolete getJobs() and var types

--- HireMe/ProviderController/ProviderJobsTableViewController.swift
+++ HireMe/ProviderController/ProviderJobsTableViewController.swift
@@ -9,129 +9,125 @@
 import UIKit
 
 class ProviderJobsTableViewController: UITableViewController {
-
-	var jobs: [Job]?
-	var completedJobs: [Job]?
-	var cancelledJobs: [Job]?
-	var types: [JobStatus]?
+    
+    fileprivate let DETAIL_TEXT_KEY = "detailText"
+    fileprivate let JOB_KEY = "job"
+    
+    var jobs: [Job]?
+    var completedJobs: [Job]?
+    var cancelledJobs: [Job]?
+    
+    private var tableViewData = [[String: Any]]()
     
     // MARK: - View controller life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tableView.hideEmptyCells()
+        tableView.hideEmptyCells()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
-		let me = BidController.shared.bids[0].bidder
-		let myjobs = JobController.shared.jobs.filter{$0.selectedBid?.bidder == me}
-		jobs = myjobs.filter {$0.status == JobStatus.open.rawValue}
-		completedJobs = myjobs.filter {$0.status == JobStatus.completed.rawValue}
-		cancelledJobs = myjobs.filter {$0.status == JobStatus.cancelled.rawValue}
-		types = getJobs()
-		tableView.reloadData()
+        
+        let me = BidController.shared.bids[0].bidder
+        let myjobs = JobController.shared.jobs.filter{ $0.selectedBid?.bidder == me }
+        jobs = myjobs.filter {$0.status == JobStatus.open.rawValue}
+        completedJobs = myjobs.filter { $0.status == JobStatus.completed.rawValue }
+        cancelledJobs = myjobs.filter { $0.status == JobStatus.cancelled.rawValue }
+        initializeTableViewData()
+        tableView.reloadData()
     }
-	
-	// MARK: - UITableViewDataSource methods
-
-	override func numberOfSections(in tableView: UITableView) -> Int {
-		return getJobs().count
-	}
-
-	override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-		guard let types = types else { return "" }
-		if section == 0 && types.contains(JobStatus.open) {
-			return "Current Jobs"
-		} else if (section == 1 && types.contains(JobStatus.completed) && types.contains(JobStatus.open)) ||
-			section == 0 && types.contains(JobStatus.completed) {
-			return "Completed Jobs"
-		} else if (section == 2 && types.contains(JobStatus.cancelled) && types.contains(JobStatus.completed) && types.contains(JobStatus.open)) ||
-			(section == 1 && (types.contains(JobStatus.completed) || types.contains(JobStatus.open)) && types.contains(JobStatus.cancelled)) ||
-			(section == 0 && !types.contains(JobStatus.open) && !types.contains(JobStatus.completed) && types.contains(JobStatus.cancelled)) {
-			return "Cancelled Jobs"
-		} else {
-			return ""
-		}
-	}
-	
-	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		guard let types = types else { return 0 }
-
-		if section == 0 && types.contains(JobStatus.open) {
-			return jobs?.count ?? 0
-		} else if (section == 1 && types.contains(JobStatus.completed) && types.contains(JobStatus.open)) ||
-			section == 0 && types.contains(JobStatus.completed) {
-			return completedJobs?.count ?? 0
-		} else if (section == 2 && types.contains(JobStatus.cancelled) && types.contains(JobStatus.completed) && types.contains(JobStatus.open)) ||
-			(section == 1 && (types.contains(JobStatus.completed) || types.contains(JobStatus.open)) && types.contains(JobStatus.cancelled)) ||
-			(section == 0 && !types.contains(JobStatus.open) && !types.contains(JobStatus.completed) && types.contains(JobStatus.cancelled)) {
-			return cancelledJobs?.count ?? 0
-		} else {
-			return 0
-		}
-	}
-	
-	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		guard let cell = tableView.dequeueReusableCell(withIdentifier: "jobCell"),
-			let types = types else { return UITableViewCell() }
-
-		if indexPath.section == 0 && types.contains(JobStatus.open) {
-			cell.textLabel?.text = jobs?[indexPath.row].name
-			cell.detailTextLabel?.text = jobs?[indexPath.row].industry
-		} else if (indexPath.section == 1 && types.contains(JobStatus.completed) && types.contains(JobStatus.open)) ||
-			indexPath.section == 0 && types.contains(JobStatus.completed) {
-			cell.textLabel?.text = completedJobs?[indexPath.row].name
-			cell.detailTextLabel?.text = completedJobs?[indexPath.row].industry
-		} else if (indexPath.section == 2 && types.contains(JobStatus.cancelled) && types.contains(JobStatus.completed) && types.contains(JobStatus.open)) ||
-			(indexPath.section == 1 && (types.contains(JobStatus.completed) || types.contains(JobStatus.open)) && types.contains(JobStatus.cancelled)) ||
-			(indexPath.section == 0 && !types.contains(JobStatus.open) && !types.contains(JobStatus.completed) && types.contains(JobStatus.cancelled)) {
-			cell.textLabel?.text = cancelledJobs?[indexPath.row].name
-			cell.detailTextLabel?.text = cancelledJobs?[indexPath.row].industry
-		}
-
-		return cell
-	}
+    
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let navController = segue.destination as? UINavigationController,
+            let vc = navController.childViewControllers.first as? ProviderJobDetailTableViewController,
+            let cell = sender as? UITableViewCell,
+            let indexPath = tableView.indexPath(for: cell),
+            let job = job(atIndexPath: indexPath) {
+            vc.job = job
+        }
+    }
+    
+    // MARK: - UITableViewDataSource
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return tableViewData.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return tableViewData[section][SECTION_TITLE_KEY] as? String
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let rows = tableViewData[section][ROWS_KEY] as? [[String: Any]] else { return 0 }
+        
+        return rows.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "jobCell"),
+            let job = job(atIndexPath: indexPath) else {
+                return UITableViewCell()
+        }
+        
+        cell.textLabel?.text = job.name
+        cell.detailTextLabel?.text = job.industry
+        
+        return cell
+    }
     
     
-    // MARK: - Custom functions
-
-	func getJobs() -> [JobStatus] {
-		if let jobs = jobs,
-			let completedJobs = completedJobs,
-			let cancelledJobs = cancelledJobs,
-			jobs.count > 0,
-			cancelledJobs.count > 0,
-			completedJobs.count > 0 {
-			return [JobStatus.cancelled, JobStatus.completed, JobStatus.open]
-		} else if let jobs = jobs,
-			let completedJobs = completedJobs,
-			jobs.count > 0,
-			completedJobs.count > 0 {
-			return [JobStatus.completed, JobStatus.open]
-		} else if let jobs = jobs,
-			let cancelledJobs = cancelledJobs,
-			jobs.count > 0,
-			cancelledJobs.count > 0 {
-			return [JobStatus.cancelled, JobStatus.open]
-		} else if let completedJobs = completedJobs,
-			let cancelledJobs = cancelledJobs,
-			cancelledJobs.count > 0,
-			completedJobs.count > 0 {
-			return [JobStatus.cancelled, JobStatus.completed]
-		} else if let jobs = jobs,
-			jobs.count > 0 {
-			return [JobStatus.open]
-		} else if let completedJobs = completedJobs,
-			completedJobs.count > 0 {
-			return [JobStatus.completed]
-		} else if let cancelledJobs = cancelledJobs,
-			cancelledJobs.count > 0 {
-			return [JobStatus.cancelled]
-		} else {
-			return []
-		}
-	}
-
+    // MARK: - Private functions
+    
+    private func initializeTableViewData() {
+        if let jobs = jobs {
+            addTableViewDataSection(title: "Current Jobs", with: jobs)
+        }
+        
+        if let completedJobs = completedJobs {
+            addTableViewDataSection(title: "Completed Jobs", with: completedJobs)
+        }
+        
+        if let cancelledJobs = cancelledJobs {
+            addTableViewDataSection(title: "Cancelled Jobs", with: cancelledJobs)
+        }
+    }
+    
+    private func addTableViewDataSection(title: String, with jobs: [Job]) {
+        var section = tableViewDataSection(title: title)
+        
+        
+        if var rows = section[ROWS_KEY] as? [[String: Any]] {
+            for job in jobs {
+                rows.append(tableViewDataRow(for: job))
+            }
+            
+            section[ROWS_KEY] = rows
+            
+            if rows.count > 0 {
+                tableViewData.append(section)
+            }
+        }
+    }
+    
+    private func tableViewDataSection(title: String) -> [String: Any] {
+        return [
+            SECTION_TITLE_KEY: title,
+            ROWS_KEY: [[String: Any]]()
+        ]
+    }
+    
+    private func tableViewDataRow(for job: Job) -> [String: Any] {
+        return [JOB_KEY: job]
+    }
+    
+    private func job(atIndexPath indexPath: IndexPath) -> Job? {
+        guard let rows = tableViewData[indexPath.section][ROWS_KEY] as? [[String: Any]],
+            let job = rows[indexPath.row][JOB_KEY] as? Job else { return nil }
+        
+        return job
+    }
+    
 }
