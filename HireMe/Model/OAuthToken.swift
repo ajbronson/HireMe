9749dca8bd7562fbf6ebab6dadd75cf1f4de990e
Nathan M Johnson <nate8etan@gmail.com>
2017-04-01 14:24:25 -0600

    OAuthToken: replaced expiresIn with creationDate and expirationDate, added var isExpired, now conforms to CustomStringConvertible and implements description

--- HireMe/Model/OAuthToken.swift
+++ HireMe/Model/OAuthToken.swift
@@ -8,19 +8,24 @@
 
 import UIKit
 
-class OAuthToken {
+class OAuthToken : CustomStringConvertible {
     var accessToken: String
     var refreshToken: String
     var scope: String
     var tokenType: String
-    var expiresIn: Int
+    var creationDate: Date
+    var expirationDate: Date
+    var isExpired: Bool {
+        // If ordered ascending, creation date is earlier than expiration date
+        return creationDate.compare(expirationDate) != .orderedAscending
+    }
     
     init?(json: [String: Any]) {
         guard let accessToken = json["access_token"] as? String,
             let refreshToken = json["refresh_token"] as? String,
             let scope = json["scope"] as? String,
             let tokenType = json["token_type"] as? String,
-            let expiresIn = json["expires_in"] as? Int else {
+            let expiresIn = json["expires_in"] as? Double else {
             return nil
         }
         
@@ -28,10 +33,18 @@ class OAuthToken {
         self.refreshToken = refreshToken
         self.scope = scope
         self.tokenType = tokenType
-        self.expiresIn = expiresIn
+        self.creationDate = Date()
+        self.expirationDate = creationDate.addingTimeInterval(expiresIn)
     }
     
     func authorization() -> String {
         return "\(tokenType) \(accessToken)"
+    }
+    
+    var description: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd-MMM-yyyy h:mm:ss a" // 02-Apr-2017 3:45:21 PM
+        
+        return "<\(authorization())> expires at \(dateFormatter.string(from: expirationDate))"
     }
 }
