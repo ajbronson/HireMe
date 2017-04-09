df6a03d3d0df2fd38a2083ca9743f105c7d2ad87
Nathan M Johnson <nate8etan@gmail.com>
2017-04-09 05:33:31 -0600

    User: now conforms to CustomStringConvertible, added properties username and email, created init(dictionary:); created APIClient with getUser(completionHandler:)

--- HireMe/HelperFiles/APIClient.swift
+++ HireMe/HelperFiles/APIClient.swift
@@ -8,6 +8,31 @@
 
 import Foundation
 
-class APIClient {
-    
+struct APIClient {
+    static func getUser(completionHandler: @escaping (User?, Error?) -> Void) {
+        let url = NetworkConroller.url(base: BASE_URL, pathParameters: ["whoami"])
+        NetworkConroller.request(url, method: .Get) { (request, error) in
+            guard let urlRequest = request else {
+                completionHandler(nil, error)
+                return
+            }
+            
+            NetworkConroller.performURLRequest(urlRequest, completion: { (data, error2) in
+                if let err = error2 {
+                    completionHandler(nil, err)
+                } else {
+                    guard let json = data?.toJSON(), let responseDict = json as? [String: Any] else {
+                        completionHandler(nil, NetworkError.deserializeJSON)
+                        return
+                    }
+                    
+                    do {
+                        completionHandler(try User(dictionary: responseDict), nil)
+                    } catch let initError {
+                        completionHandler(nil, initError)
+                    }
+                }
+            })
+        }
+    }
 }
