637b4b5d6869a2b91a305657156c05da85bb8386
Nathan M Johnson <nate8etan@gmail.com>
2017-04-10 22:21:43 -0600

    APIClient: created performURLRequest(forEndpoint:completionHandler:)

--- HireMe/HelperFiles/APIClient.swift
+++ HireMe/HelperFiles/APIClient.swift
@@ -10,13 +10,35 @@ import Foundation
 
 struct APIClient {
     static func getUser(completionHandler: @escaping (User?, Error?) -> Void) {
-        let url = NetworkConroller.url(base: BASE_URL, pathParameters: ["whoami/"])
+        performURLRequest(forEndpoint: "whoami/") { (responseDict, error) in
+            guard let dict = responseDict else {
+                completionHandler(nil, error)
+                return
+            }
+            
+            do {
+                completionHandler(try User(dictionary: dict), nil)
+            } catch let initError {
+                completionHandler(nil, initError)
+            }
+        }
+    }
+    
+    /**
+     Performs a GET request.
+     
+     - Parameter endpoint: The API endpoint to hit
+     - Parameter completionHandler: A Dictionary and an Error will never be returned simultaneously. Either one will be returned or the other.
+     - Returns: A dictionary of the response JSON
+     */
+    private static func performURLRequest(forEndpoint endpoint: String, completionHandler: @escaping ([String: Any]?, Error?) -> Void) {
+        let url = NetworkConroller.url(base: BASE_URL, pathParameters: [endpoint])
         NetworkConroller.request(url, method: .Get) { (request, error) in
             guard let urlRequest = request else {
                 completionHandler(nil, error)
                 return
             }
-
+            
             NetworkConroller.performURLRequest(urlRequest, completion: { (data, error2) in
                 if let err = error2 {
                     completionHandler(nil, err)
@@ -26,11 +48,7 @@ struct APIClient {
                         return
                     }
                     print(responseDict) // DEBUG
-                    do {
-                        completionHandler(try User(dictionary: responseDict), nil)
-                    } catch let initError {
-                        completionHandler(nil, initError)
-                    }
+                    completionHandler(responseDict, nil)
                 }
             })
         }
